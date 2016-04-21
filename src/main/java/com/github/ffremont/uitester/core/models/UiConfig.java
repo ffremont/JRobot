6c334cc761379d5587d50dd3eeaa7d172c4763b6
Florent FREMONT <ff.fremont.florent@gmail.com>
2016-04-21 06:38:01 +0200

    first commit

---
+++ src/main/java/com/github/ffremont/uitester/core/models/UiConfig.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.ffremont.uitester.core.models;
+
+/**
+ *
+ * @author florent
+ */
+public class UiConfig {
+    private String id;
+    private String label;
+    private Dimension screen;
+}
